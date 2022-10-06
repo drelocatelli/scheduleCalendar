e82e51e1b436a150f895eea4cef75c976ba93d3b
drelocatelli <andressalocatelli@hotmail.com>
2022-10-06 14:35:30 -0300

    setting up modal shedule

--- src/components/period/modal/scheduleEvent/index.tsx
+++ src/components/period/modal/scheduleEvent/index.tsx
@@ -1,14 +1,24 @@
 import './index.css';
+import {FiClock} from 'react-icons/fi';
 
 export default function ScheduleEvent(props: {week: string, hour: number}) {
+    const {hour, week} = props;
+    
     return(
         <div className='schedule-event'>
             <form>
-                <input type="text" placeholder="Add title" />
-                <button type='button'>Event</button>
-                <button type='button'>Task</button>
-                <button type='button'>Active</button>
-                <button type='button'>Busy</button>
+                <input type="text" className='spacing' placeholder="Add title" />
+                <div className="time-schedule basic-spacing">
+                    <div className="timer icon">
+                        <FiClock />
+                    </div>
+                    <div className=''>
+                        {week}, {hour}:00 {hour >= 12 ? 'PM' : 'AM'}
+                    </div>
+                </div>
+                <div className="basic-spacing">
+                    to do busy function
+                </div>
             </form>
         </div>
     );
