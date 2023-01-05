3454be3bd3f7c36396dcca8c1bdfb0337f0be810
drelocatelli <andressalocatelli@hotmail.com>
2023-01-05 10:56:16 -0300

    remove recoil

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import { Home } from './pages/home';
 import './assets/styles/basic.css';
-import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <RecoilRoot>
+    <>
         <Home />
-    </RecoilRoot>
+    </>
 );
